398fc5f9fcdf1788e737e35d9ef72c0faf38f62f
spot1-tech <sxxna1001@gmail.com>
2026-06-15 18:48:20 +0300

    added server configuration in .env

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://2.56.241.145:30080',
         changeOrigin: true,
         rewrite: (path) => path
       }
